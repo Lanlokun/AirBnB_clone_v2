40da09a8529b673869e6a86c31b0c3c972f6e28b
malik <mlanlokun@gmail.com>
2023-04-10 16:18:40 +0200

    added print statements

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,18 +11,30 @@ env.user = "ubuntu"
 def do_deploy(archive):
     """ Function that distributes an archive to your web servers """
     if isfile(archive) is False:
+        print("File not found")
         return False
     try:
         put(archive, "/tmp/")
+        print("File uploaded")
         file = archive.split("/")[-1]
+        print(file)
         folder = ("/data/web_static/releases/" + file.split(".")[0])
+        print(folder)
         run("mkdir -p {}".format(folder))
+        print("Folder created")
         run("tar -xzf /tmp/{} -C {}".format(file, folder))
+        print("Uncompressed")
         run("rm /tmp/{}".format(file))
+        print("File deleted")
         run("mv {}/web_static/* {}".format(folder, folder))
+        print("Moved")
         run("rm -rf {}/web_static".format(folder))
+        print("Removed")
         run("rm -rf /data/web_static/current")
+        print("Removed")
         run("ln -s {} /data/web_static/current".format(folder))
+        print("Linked")
+        print("New version deployed!")
         return True
     except:
         return False
