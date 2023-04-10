502756dd3c7fafaae474c8074d0d8b24beba348c
malik <mlanlokun@gmail.com>
2023-04-10 07:49:14 +0200

    fixed pep8 vsalidation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-""" Fabric script that generates a .tgz archive from the contents of the web_static  """
+""" Fabric script that generates a .tgz archive
+ from the contents of the web_static """
 
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
-    """ Function that generates a .tgz archive from the contents of the web_static """
+    """ Function that generates a .tgz archive
+    from the contents of the web_static """
 
     local("mkdir -p versions")
     time = datetime.now().strftime("%Y%m%d%H%M%S")
     file = "versions/web_static_{}.tgz".format(time)
     local("tar -cvzf {} web_static".format(file))
 
+
 if __name__ == "__main__":
     do_pack()
-
