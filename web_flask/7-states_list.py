285e55105bdf226e26537c1ef20661612e1058b8
malik <mlanlokun@gmail.com>
2023-04-23 13:41:19 +0200

    switched teardown

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,13 +9,21 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext()
-def teardown_db(exception):
-    """ Function that closes the database """
-    storage.close()
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ Function that returns a list of states """
     states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
+
+
+
+@app.teardown_appcontext()
+def teardown_db(exception):
+    """ Function that closes the database """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
+    
